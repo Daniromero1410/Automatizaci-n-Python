bb8b59b00c99f6ed4743cf39fcf62fc7de573d6f
Daniel Romero ;D <130934263+Daniromero1410@users.noreply.github.com>
2023-12-18 11:32:00 -0500

    Update unir_pdfs.py

--- unir_pdfs.py
+++ unir_pdfs.py
@@ -12,7 +12,7 @@ def unir_pdfs(output_path,documents):
         pdf_final.append(document)
     pdf_final.write(output_path)
 
-    
+
 #Front
 st.image('assets/combine-pdf.png')
 st.header('Unir PDF')
@@ -21,3 +21,14 @@ st.subheader('Adjuntar Pdfs para unir')
 pdf_adjuntos = st.file_uploader(label="",accept_multiple_files=True)
 
 unir = st.button(label="Unir PDFS") #Crea un boton llamado "Unir PDf"
+
+if unir:
+    if len(pdf_adjuntos)<=1:
+        st.warning("Debes Adjuntar mas de un PDF")
+    else:
+        unir_pdfs(output_pdf,pdf_adjuntos)
+        st.success("Desde aqui puede descargar el PDF Final")
+        with open(output_pdf,'rb') as file:
+            pdf_data = file.read()
+        st.download_button(label="Descargar PDF final",data=pdf_data,file_name= "Pdf_:final")
+    
