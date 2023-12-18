d13afa3181a136a7b04c80d42849d3b566c8847a
Daniel Romero ;D <130934263+Daniromero1410@users.noreply.github.com>
2023-12-18 11:27:28 -0500

    Update unir_pdfs.py

--- unir_pdfs.py
+++ unir_pdfs.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import PyPDF2
 
+#Variables
+output_pdf = ("documents/pdf_final.pdf")
+
+#Funciones
+def unir_pdfs(output_path,documents):
+    pdf_final = PyPDF2.PdfMerger()
+
+    for document in documents:
+        pdf_final.append(document)
+    pdf_final.write(output_path)
+
+    
 #Front
 st.image('assets/combine-pdf.png')
 st.header('Unir PDF')
